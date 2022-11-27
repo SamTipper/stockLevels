3bb4866dc7a5542f7e03dc3627aa9b5d345fe623
Sam <97979690+SamTipper@users.noreply.github.com>
2022-11-27 22:42:08 +0000

    Update access.js

--- js/access.js
+++ js/access.js
@@ -3,7 +3,6 @@ const tick = document.getElementById("tick");
 button.addEventListener("click", addKey);
 
 function addKey(){
-    console.log("hi");
     localStorage.setItem("api-key", document.getElementById("api-key").value);
     tick.style.display = "inline-block";
 }
